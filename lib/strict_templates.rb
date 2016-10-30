d606e0928d03a01891e92438ff142e3d51becbd6
Kelly Sutton <michael.k.sutton@gmail.com>
2016-10-30 16:01:57 -0700

    Remove the empty Collector class

--- lib/strict_templates.rb
+++ lib/strict_templates.rb
@@ -1,6 +1,5 @@
 require 'strict_templates/version'
 require 'strict_templates/state_collector'
-require 'strict_templates/collector'
 require 'strict_templates/subscriber'
 require 'strict_templates/railtie' if defined?(Rails)
 
