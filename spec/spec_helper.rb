c960a8988d0850267cae711edff6e9baadd887e5
Kelly Sutton <michael.k.sutton@gmail.com>
2016-10-30 21:57:46 -0700

    Implement tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,3 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "strict_templates"
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'strict_templates'
+require 'active_support'
--- spec/strict_templates_spec.rb
+++ spec/strict_templates_spec.rb
@@ -1,7 +1,24 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe StrictTemplates do
-  it "has a version number" do
+  let(:test_class) { Struct.new(:name) { include StrictTemplates::Concern } }
+  let(:test_instance) { test_class.new }
+
+  it 'has a version number' do
     expect(StrictTemplates::VERSION).not_to be nil
+  end
+
+  it 'responds to #render' do
+    expect(test_instance).to respond_to(:render).with_unlimited_arguments
+  end
+
+  it 'raises SQLPerformedWithinTemplateError' do
+    allow(ActiveSupport::Notifications).to receive(:subscribed) do |callback, namespace|
+      callback.call(namespace, Time.now - 1, Time.now, SecureRandom.hex(10), { name: 'Post Load' })
+    end
+
+    expect{
+      test_instance.render
+    }.to raise_error(StrictTemplates::SQLPerformedWithinTemplateError)
   end
 end
